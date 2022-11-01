93e81f5479b8ca962f3d5edbf7f09504667f90c5
Dmytro Matsehora <ninjawithoutface@gmail.com>
2022-11-01 23:01:55 +0200

    fix home route

--- client/src/components/mainContent/mainContent.tsx
+++ client/src/components/mainContent/mainContent.tsx
@@ -8,14 +8,14 @@ import { Phone } from '../../types/Phone';
 import './mainContent.scss';
 
 type Props = {
-  phones: Phone[] | null;
+  phones: Phone[];
 };
 
 export const Main: React.FC<Props> = ({ phones }) => (
   <div className="mainContent">
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="home" element={<Navigate to="/" replace />} />
+      <Route path="Home" element={<Home />} />
+      <Route path="/" element={<Navigate to="Home" replace />} />
 
       <Route path="phones">
         <Route index element={<PhonesList phones={phones} />} />
