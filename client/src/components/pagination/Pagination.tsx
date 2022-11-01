d233c2660ce77122495386a1dcfd70612e5597fe
Anton Liada <liadaantom@gmail.com>
2022-11-01 22:30:53 +0200

    pagination was implemented

---
+++ client/src/components/pagination/Pagination.tsx
@@ -0,0 +1,83 @@
+import classNames from 'classnames';
+import { useLocation } from 'react-router-dom';
+import { ButtonClassModifier, ButtonClassType } from '../../enums/buttonEnum';
+
+type Props = {
+  phonesPerPage: number,
+  totalPhones?: number,
+  paginate: (value: number) => void,
+  currentPage: number,
+  nextPage: () => void,
+  prevPage: () => void,
+};
+
+export const Pagination: React.FC<Props> = ({
+  phonesPerPage,
+  totalPhones,
+  paginate,
+  currentPage,
+  nextPage,
+  prevPage,
+}) => {
+  const pageNumbers = [];
+  const selectedPage = useLocation().pathname.split('/Phones/')[1];
+
+  if (totalPhones) {
+    const totalPages = Math.ceil(totalPhones / phonesPerPage);
+
+    for (let i = 1; i <= totalPages; i += 1) {
+      pageNumbers.push(i);
+    }
+  }
+
+  return (
+    <div className="pagination">
+      <a // eslint-disable-line jsx-a11y/anchor-has-content
+        href={`#/Phones/${currentPage}`}
+        aria-label="link"
+        className={classNames(
+          'pagination__prev-btn',
+          ButtonClassType.SECONDARY,
+          ButtonClassModifier.PREV_PAGE,
+        )}
+        onClick={prevPage}
+      />
+
+      <ul className="pagination__list">
+        {pageNumbers.map(number => {
+          return (
+            <li
+              className="pagination__page-number"
+              key={number}
+            >
+              <a
+                href={`#/Phones/${currentPage}`}
+                className={classNames(
+                  ButtonClassType.SECONDARY,
+                  ButtonClassModifier.NUM_PAGE,
+                  { 'pagination__active-button': (+selectedPage === number) },
+                )}
+                onClick={() => {
+                  paginate(number);
+                }}
+              >
+                {number}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <a // eslint-disable-line jsx-a11y/anchor-has-content
+        href={`#/Phones/${currentPage}`}
+        aria-label="link"
+        className={classNames(
+          'pagination__next-btn',
+          ButtonClassType.SECONDARY,
+          ButtonClassModifier.NEXT_PAGE,
+        )}
+        onClick={nextPage}
+      />
+    </div>
+  );
+};
--- client/src/components/phonesList/PhonesList.tsx
+++ client/src/components/phonesList/PhonesList.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Phone } from '../../types/Phone';
+import { Pagination } from '../pagination/Pagination';
 import { PhonesListItem } from './PhonesList__Item';
 
 type Props = {
@@ -6,11 +8,44 @@ type Props = {
 };
 
 export const PhonesList: React.FC<Props> = ({ phones }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [phonesPerPage, setPhonesPerPage] = useState(8);
+
+  const lastPhoneIndex = currentPage * phonesPerPage;
+  const firstPhoneIndex = lastPhoneIndex - phonesPerPage;
+  const currentPhones = phones?.slice(firstPhoneIndex, lastPhoneIndex);
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const nextPage = () => {
+    if (phones && lastPhoneIndex < phones.length) {
+      setCurrentPage(prev => prev + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (phones && firstPhoneIndex > 1) {
+      setCurrentPage(prev => prev - 1);
+    }
+  };
+
   return (
     <div className="container">
       <div className="grid__cards">
-        {phones?.map(phone => <PhonesListItem key={phone.id} phone={phone} />)}
+        {currentPhones?.map(phone => (
+          <PhonesListItem key={phone.id} phone={phone} />))}
       </div>
+
+      <Pagination
+        phonesPerPage={phonesPerPage}
+        totalPhones={phones?.length}
+        paginate={paginate}
+        currentPage={currentPage}
+        nextPage={nextPage}
+        prevPage={prevPage}
+      />
     </div>
   );
 };
