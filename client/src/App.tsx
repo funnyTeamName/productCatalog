b593707f2a47d8a77e90905c81a9826ad7f56aa0
Dmytro Matsehora <ninjawithoutface@gmail.com>
2022-11-01 21:00:36 +0200

    footer fixed & separate main component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
-import { PhonesList } from './components/phonesList/PhonesList';
 import { Phone } from './types/Phone';
-import { NotFoundPage } from './pages/NotFoundPage';
-import { Home } from './pages/Home';
-import { Tablets } from './pages/Tablets';
-import { Accessories } from './pages/Accessories';
+import { Main } from './components/mainContent/mainContent';
 
 export const App = () => {
   const [phones, setPhones] = useState<Phone[] | null>(null);
@@ -21,22 +16,7 @@ export const App = () => {
   return (
     <div>
       <Header />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="home" element={<Navigate to="/" replace />} />
-
-        <Route path="phones">
-          <Route index element={<PhonesList phones={phones} />} />
-          <Route path=":pagId" element={<PhonesList phones={phones} />} />
-        </Route>
-
-        <Route path="tablets" element={<Tablets />} />
-        <Route path="accessories" element={<Accessories />} />
-
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-
+      <Main phones={phones} />
       <Footer />
     </div>
   );
