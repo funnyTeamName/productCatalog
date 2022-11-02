f3801a69348ae58a909be670d54bfdb9f1ee2712
Maks Sobko <tiserettt@gmail.com>
2022-11-02 23:57:44 +0200

    created endpoint for /products/:productId

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,6 +19,18 @@ app.get('/products', (req, res) => {
   res.send(JSON.parse(phones));
 });
 
+app.get('/products/:productId', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('./data/phones.json', 'utf-8'));
+
+  const { name } = data.find(phone => phone.id === req.params.productId);
+
+  const phonePath = name.split(' ').join('-');
+
+  const foundPhone = fs.readFileSync(`./data/phones/${phonePath}.json`, 'utf-8');
+
+  res.send(JSON.parse(foundPhone));
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server listening on http://localhost:${PORT}`);
