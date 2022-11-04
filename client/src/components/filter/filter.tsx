b1debff8f104ec66e1d15a219299d192586697cf
Anton Liada <liadaantom@gmail.com>
2022-11-04 17:52:33 +0200

    fitler was fixed

--- client/src/components/filter/filter.tsx
+++ client/src/components/filter/filter.tsx
@@ -46,68 +46,77 @@ export const Filter: React.FC<Props> = ({
   const currentUrl = window.location.href.split('Phones/')[0];
 
   return (
-    <div className="filter page__filter">
-      <div className="filter__logo">
-        <Link to="/" className="filter__logo-home" />
-        <Link to="/" className="filter__logo-vector" />
-        <p className="filter__logo-text">Phones</p>
-      </div>
-
-      <h1 className="filter__title">Mobile phones</h1>
-      <div className="device__count device__count-margin">
-        {`${length} models`}
-      </div>
-
-      <div className="filter__sorts">
-        <div className="sorts__item sorts__item-margin">
-          <p className="sorts__text">Sort by</p>
-          <select
-            name="sort"
-            id="sort"
-            className="sorts__select"
-            value={filterType}
-            onChange={event => handleFilter(event.target.value)}
-          >
-            <option value={FilterType.NEWEST} className="sorts__option">
-              Newest
-            </option>
-            <option value={FilterType.OLDEST} className="sorts__option">
-              Oldest
-            </option>
-            <option value={FilterType.ASC} className="sorts__option">
-              Ascending price
-            </option>
-            <option value={FilterType.DESC} className="sorts__option">
-              Descending price
-            </option>
-          </select>
+    <div className="grid">
+      <div
+        className="
+          filter
+          page__filter
+          grid__item--mobile-1-4
+        "
+      >
+        <div className="filter__logo">
+          <Link to="/" className="filter__logo-home" />
+          <Link to="/" className="filter__logo-vector" />
+          <p className="filter__logo-text">Phones</p>
         </div>
 
-        <div className="sorts__item">
-          <p className="sorts__text">Items on page</p>
-          <select
-            name="show"
-            id="show"
-            className="sorts__select"
-            value={phonesPerPage}
-            onChange={event => {
-              setPhonesPerPage(+event.target.value);
-              setCurrentPage(1);
+        <h1 className="filter__title">Mobile phones</h1>
+        <div className="device__count device__count-margin">
+          {`${length} models`}
+        </div>
 
-              window.location.replace(
-                currentUrl.endsWith('Phones')
-                  ? currentUrl
-                  : `${currentUrl.split('Phones/')[0]}Phones`,
-              );
-            }}
-          >
-            <option value="8" className="sorts__option">8</option>
-            <option value="12" className="sorts__option">12</option>
-            <option value="16" className="sorts__option">16</option>
-            <option value="20" className="sorts__option">20</option>
-          </select>
+        <div className="filter__sorts">
+          <div className="sorts__item sorts__item-margin">
+            <p className="sorts__text">Sort by</p>
+            <select
+              name="sort"
+              id="sort"
+              className="sorts__select"
+              value={filterType}
+              onChange={event => handleFilter(event.target.value)}
+            >
+              <option value={FilterType.NEWEST} className="sorts__option">
+                Newest
+              </option>
+              <option value={FilterType.OLDEST} className="sorts__option">
+                Oldest
+              </option>
+              <option value={FilterType.ASC} className="sorts__option">
+                Ascending price
+              </option>
+              <option value={FilterType.DESC} className="sorts__option">
+                Descending price
+              </option>
+            </select>
+          </div>
+
+          <div className="sorts__item">
+            <p className="sorts__text">Items on page</p>
+            <select
+              name="show"
+              id="show"
+              className="sorts__select"
+              value={phonesPerPage}
+              onChange={event => {
+                setPhonesPerPage(+event.target.value);
+                setCurrentPage(1);
+
+                window.location.replace(
+                  currentUrl.endsWith('Phones')
+                    ? currentUrl
+                    : `${currentUrl.split('Phones/')[0]}Phones`,
+                );
+              }}
+            >
+              <option value="8" className="sorts__option">8</option>
+              <option value="12" className="sorts__option">12</option>
+              <option value="16" className="sorts__option">16</option>
+              <option value="20" className="sorts__option">20</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
+
   );
 };
