27a539ff3b3c8162be1c5336f989b8cd5bc5ab32
Anton Liada <liadaantom@gmail.com>
2022-11-06 22:05:02 +0200

    swiper for all devices was implemented

--- client/src/components/ModelsSection/ModelsSection.tsx
+++ client/src/components/ModelsSection/ModelsSection.tsx
@@ -1,10 +1,15 @@
-import classNames from 'classnames';
+import {
+  Navigation,
+  Pagination,
+} from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/effect-cards';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import { PhonesListItem } from '../ProductList/ProductListItem';
-import { ButtonClassModifier, ButtonClassType } from '../../enums/ButtonEnum';
 import { Phone } from '../../types/Phone';
 import './ModelsSection.scss';
-import 'swiper/css';
 
 type Props = {
   phones: Phone[];
@@ -23,69 +28,49 @@ export const ModelsSection: React.FC<Props> = ({
   likedPhones,
   setLikedPhones,
 }) => {
-  const newModels = phones
-    .sort((p1, p2) => p2.year - p1.year)
-    .slice(0, 5);
+  const newModels = phones.sort((p1, p2) => p2.year - p1.year);
 
   return (
     <section className="home__section-models product-slider">
-      <div className="grid product-slider__grid-container container">
+      <div className="product-slider__container">
         <h2
-          className="
-          title
-          product-slider__title
-          grid__item--mobile-1-2
-          grid__item--iPad-1-12
+          className="title product-slider__title
         "
         >
           Brand new models
         </h2>
 
-        <div
-          className="
-          product-slider__button-wrapper
-          product-slider__button-wrapper--position
-        "
+        <Swiper
+          slidesPerView="auto"
+          spaceBetween={16}
+          loop
+          loopFillGroupWithBlank
+          navigation
+          modules={[Pagination, Navigation]}
+          breakpoints={{
+            1200: { slidesPerView: 4 },
+            1150: { slidesPerView: 4.5 },
+            1024: { slidesPerView: 4.1 },
+            900: { slidesPerView: 3.5 },
+            768: { slidesPerView: 2.9 },
+            580: { slidesPerView: 2.5 },
+            500: { slidesPerView: 2.2 },
+            420: { slidesPerView: 1.9 },
+            375: { slidesPerView: 1.6 },
+            320: { slidesPerView: 1.4 },
+          }}
         >
-          <a // eslint-disable-line jsx-a11y/anchor-has-content
-            href="/"
-            aria-label="link"
-            className={classNames(
-              'grid__item--mobile-5-6',
-              'home-button-slider',
-              ButtonClassType.SECONDARY,
-              ButtonClassModifier.PREV_PAGE,
-            )}
-          />
-
-          <a // eslint-disable-line jsx-a11y/anchor-has-content
-            href="/"
-            aria-label="link"
-            className={classNames(
-              'grid__item--mobile-7-8',
-              'home-button-slider',
-              ButtonClassType.SECONDARY,
-              ButtonClassModifier.NEXT_PAGE,
-            )}
-          />
-        </div>
-      </div>
-
-      <div className="product-slider-cards">
-        <Swiper slidesPerView={4} spaceBetween={16}>
           {newModels.map(phone => (
             <SwiperSlide>
-              <div className="product-slider-cards__item">
-                <PhonesListItem
-                  key={phone.id}
-                  phone={phone}
-                  selectedPhones={selectedPhones}
-                  setSelectedPhones={setSelectedPhones}
-                  setPhoneId={setPhoneId}
-                  likedPhones={likedPhones}
-                  setLikedPhones={setLikedPhones}
-                />
-              </div>
+              <PhonesListItem
+                key={phone.id}
+                phone={phone}
+                selectedPhones={selectedPhones}
+                setSelectedPhones={setSelectedPhones}
+                setPhoneId={setPhoneId}
+                likedPhones={likedPhones}
+                setLikedPhones={setLikedPhones}
+              />
             </SwiperSlide>
           ))}
         </Swiper>
